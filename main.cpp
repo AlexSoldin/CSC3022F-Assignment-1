ef73609ac2874aa9123da8c6d276535264c395be
Alex Soldin <alex.soldin@gmail.com>
2020-02-15 15:20:10 +0200

    Switch case for menu working

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
     std::string in = "Start";
 
     while(in.compare("q")!=0){
+        in = "Start";
         cout << "Choose an option from the following:"
                 "\n1: Add student"
                 "\n2: Read database"
@@ -19,6 +20,27 @@ int main() {
 
         cin >> in;
         cout << "\n";
+
+        switch(in[0]) {
+            case '1':
+                cout << "Option 1\n\n";
+                break;
+            case '2':
+                cout << "Option 2\n\n";
+                break;
+            case '3':
+                cout << "Option 3\n\n";
+                break;
+            case '4':
+                cout << "Option 4\n\n";
+                break;
+            case '5':
+                cout << "Option 5\n\n";
+                break;
+
+            default : cout << "Ivalid option. Please try again.\n\n";
+        }
+
     }
 
     std::cout << "Hello, World!" << std::endl;
